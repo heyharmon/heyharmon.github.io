b43ae82d2a1b197d566f23d358f218b6be33940f
Ryan Harmon <readyharmon@gmail.com>
2016-07-13 19:01:48 -0500

    Dynamic personalization V1

--- assets/js/personalization.js
+++ assets/js/personalization.js
@@ -1,23 +1,41 @@
 /**
  * Setup elements
  */
-function personalizeHomeJumbotron(id, newColor)
+function personalizeHomeJumbotron(id, bgColor, h1Text, pText )
 {
- var Element = document.getElementById(id);
- Element.style.backgroundColor = newColor;
+  var element = document.getElementById(id),
+      container = element.children[0],
+      children = container.children;
+  element.style.backgroundColor = bgColor;
+  children[0].innerHTML = h1Text;
+  children[1].innerHTML = pText;
 }
 
 /**
- * Page view cookie
- * Read cookies, if match run personalization
+ * Evaluate bloomio_group cookie
+ * Find value of bloomio_group, run personalization
  */
+
+// Check if page is loaded?
+// https://davidwalsh.name/document-readystate
+
 (function() {
-  var pb = this;
-  var cookie = document.cookie.replace(/(?:(?:^|.*;\s*)bloomio_auto_loan\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  var cookie = document.cookie.replace(/(?:(?:^|.*;\s*)bloomio_group\s*\=\s*([^;]*).*$)|^.*$/, "$1");
   if (cookie) {
-      pb.bloomio_auto_loan = cookie; {
-        // Run personalization
-        personalizeHomeJumbotron('home-jumbotron','salmon');
+    this.bloomio_group = cookie; {
+    console.info('Cookie found: ', document.cookie);
+      if (cookie == 1) {
+        // Run loan personalization
+        personalizeHomeJumbotron('home-jumbotron','salmon', 'Get a New Ride', 'You seem to be interested in auto loans. It is not the same as the hero on the Auto Loans page, but it is similar. Pretty cool eh?');
+        console.info('Cookie is: ', cookie);
+      } else if (cookie == 2) {
+        // Run services personalization
+        personalizeHomeJumbotron('home-jumbotron','yellow', 'Got Direct Deposit?', 'You seem to be interested in services. It is not the same as the hero on the Services page, but it is similar. Pretty cool eh?');
+        console.info('Cookie is: ', cookie);
+      } else {
+        // Do nothing
+        console.info('Cookie is: ', cookie);
       }
+    }
   }
 }());
